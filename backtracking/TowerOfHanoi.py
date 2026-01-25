dd824a58c38e93108b7ea50831cb37a142664d49
Jaiv Patel <jaivpatel402@gmail.com>
2026-01-25 17:19:49 +0530

    adding TowerOfHanoi

---
+++ backtracking/TowerOfHanoi.py
@@ -0,0 +1,11 @@
+def tower_of_hanoi(n, source, helper, destination):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {destination}")
+        return
+
+    tower_of_hanoi(n-1, source, destination, helper)
+    print(f"Move disk {n} from {source} to {destination}")
+    tower_of_hanoi(n-1, helper, source, destination)
+
+n = 3
+tower_of_hanoi(n, 'A', 'B', 'C')
